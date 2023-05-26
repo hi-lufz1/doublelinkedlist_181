f3f3e7d315787b9922edd905cfe426cb023a49c6
hi-lufz1 <latifufauzi@gmail.com>
2023-05-26 11:00:23 +0700

    2

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -10,3 +10,22 @@ public:
 	node* prev;
 
 };
+
+class DoubleLinkedlist {
+private:
+	node* START;
+public:
+	DoubleLinkedlist();
+	void addNode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+
+
+
+
+};
